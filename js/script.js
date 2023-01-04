9237beb84352339ceb808d3c08038ed95fb46c51
unknown <naimulislammd071@gmail.com>
2023-01-05 01:18:01 +0600

    hello

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+document.getElementById("calculate-btn").addEventListener('click', function(){
+  const incomeInput = document.getElementById("income");
+  const incomeInputString = incomeInput.value;
+  const incomeFinalInput = parseFloat(incomeInputString);
+  
+
+
+ // second st
+ const foodInput = document.getElementById("food");
+ const foodInputString = foodInput.value;
+ const foodFinalInput = parseFloat(foodInputString);
+//  console.log(foodFinalInput);
+
+//  2
+
+ const rentInput = document.getElementById("rent");
+ const rentInputString = rentInput.value;
+ const rentFinalInput = parseFloat(rentInputString);
+ 
+//  3
+ const clothesInput = document.getElementById("clothes");
+ const clothesInputString = clothesInput.value;
+ const clothesFinalInput = parseFloat(clothesInputString);
+
+//  sum
+const costSum = foodFinalInput + rentFinalInput + clothesFinalInput;
+
+
+// Total Expense
+const previousTotalExpense = document.getElementById("total-expense");
+const previousTotalExpenseInput = previousTotalExpense.innerText;
+previousTotalExpense.innerText = costSum;
+
+
+
+// Balance
+const balanceInput = document.getElementById("balance")
+const totallBalance = incomeFinalInput - costSum;
+balanceInput.innerText = totallBalance;
+})
+// second addEventListener save
+document.getElementById("save-btn").addEventListener('click', function(){
+  const saveBalance = document.getElementById("balance");
+  const saveBalanceString = saveBalance.innerText;
+  const saveBalanceParfloat = parseFloat(saveBalanceString);
+  // 2
+  const saveInput = document.getElementById("save");
+ const saveInputString = saveInput.value;
+ const saveFinalInput = parseFloat(saveInputString);
+ const TotalSaveBalance = saveBalanceParfloat / 100;
+ const finalTotalSaveBalance =  TotalSaveBalance * saveFinalInput;
+
+
+ const savingAmount = document.getElementById('saving-amount');
+ const previousSavingamount = savingAmount.innerText;
+ savingAmount.innerText = finalTotalSaveBalance;
+})
